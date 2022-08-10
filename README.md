99902a5008e67929318ee9df4cd403b991700250
Naman Garg <namangarg155@gmail.com>
2022-08-11 00:56:16 +0530

    Version 0.0.1

---
+++ README.md
@@ -0,0 +1,2 @@
+## Any Time Joke
+A chrome extension to fetch and display a jokes to the user using the icanhazdadjoke.com api
---
+++ jokes.js
@@ -0,0 +1,4 @@
+fetch("https://icanhazdadjoke.com/slack").then(data => data.json()).then(jokeData => {
+    const joke = jokeData.attachments[0].text;
+    document.getElementById("jokeElement").innerHTML = joke;
+})
